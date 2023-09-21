da416adce3e5c88b8923dd93d0ab4a8941885024
Matheus Biesdorf <122486659+matheusbiesdorf@users.noreply.github.com>
2023-09-21 18:50:23 -0400

    Condições pt1

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 75
+
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+
+if(vel> 60){
+    console.log('Você está rápido demais!')
+}
+
+console.log('Dirija sempre usando cinto de segurança')
