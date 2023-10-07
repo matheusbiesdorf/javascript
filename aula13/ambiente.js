ff4c34b565299fd935bb06eaf54029f2008ac4eb
Matheus Biesdorf <122486659+matheusbiesdorf@users.noreply.github.com>
2023-10-07 13:31:01 -0400

    Repetições (pt2)

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,11 +1,15 @@
 var c = 1
 while(c <= 3){
-    console.log(`Passo ${c}A`)
+    console.log(`Passo ${c} AAA`)
     c++
 }
 
 var i = 1
 do{
-    console.log(`Passo ${i}B`)
+    console.log(`Passo ${i} B`)
     i++
 }while(i <= 2) 
+
+for (a=1 ; a<4 ; a++){
+    console.log(`Passo ${a} CC`)
+}
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log("Início")
+for (a=1 ; a<=10 ; a++){
+    console.log(`Passo ${a}`)
+}
+console.log("Fim")
