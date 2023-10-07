9c13224acf70f0671d00bc0279bfba51a7250af8
Matheus Biesdorf <122486659+matheusbiesdorf@users.noreply.github.com>
2023-10-07 12:52:51 -0400

    Repetições (pt1)

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+while(c <= 3){
+    console.log(`Passo ${c}A`)
+    c++
+}
+
+var i = 1
+do{
+    console.log(`Passo ${i}B`)
+    i++
+}while(i <= 2) 
---
+++ aula13/node_9dd419a64005c.tmp.js
@@ -0,0 +1,5 @@
+var i = 1
+
+do{
+    console.log(`Passo ${i}`)
+}while(i <= 8)
