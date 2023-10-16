00af9c5a9d97731d18f82bdc07c998c9721efd17
Matheus Biesdorf <122486659+matheusbiesdorf@users.noreply.github.com>
2023-10-15 23:10:37 -0400

    Objetos

---
+++ aula17/objeto01.js
@@ -0,0 +1,13 @@
+/* OBJETOS */
+
+let amigo = {
+    nome: 'José' , 
+    sexo: 'M' , 
+    peso: 65.5 ,
+    engordar(p=0){
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+amigo.engordar(2)
+console.log (amigo.peso)
